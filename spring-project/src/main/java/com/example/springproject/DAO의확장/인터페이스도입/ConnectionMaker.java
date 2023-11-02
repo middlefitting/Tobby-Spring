d8e09e442b51ec78d62cafe94a10fe6fbdd5ed68
SeungChul Chung <wjdtmdcjf2@gmail.com>
2023-11-02 12:14:11 +0900

    feat: 인터페이스 도입 (#7)

---
+++ spring-project/src/main/java/com/example/springproject/DAO의확장/인터페이스도입/ConnectionMaker.java
@@ -0,0 +1,18 @@
+package com.example.springproject.DAO의확장.인터페이스도입;
+
+import java.sql.Connection;
+
+/**
+ * ConnectionMaker.
+ *
+ * <p>
+ *  connection을 제공한다는 인터페이스
+ * </p>
+ *
+ * @author : middlefitting
+ * @see :
+ * @since : 2023/11/02
+ */
+public interface ConnectionMaker {
+  public Connection getConnection() throws ClassNotFoundException, java.sql.SQLException;
+}
