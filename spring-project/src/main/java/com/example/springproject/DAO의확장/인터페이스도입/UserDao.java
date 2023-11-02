8b170617a8a6ac1b53e2019ac4d22d9e3416c404
middlefitting <wjdtmdcjf2@gmail.com>
2023-11-02 10:41:10 +0900

    feat: 인터페이스 도입

---
+++ spring-project/src/main/java/com/example/springproject/DAO의확장/인터페이스도입/UserDao.java
@@ -0,0 +1,72 @@
+package com.example.springproject.DAO의확장.인터페이스도입;
+
+import com.example.springproject.초난감DAO.domain.User;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * UserDao.
+ *
+ * <p>
+ *  DB 연결 작업을 분리하고 인터페이스를 동해 다양한 구현체들을 사용할 수 있도록 한 UserDao
+ * </p>
+ *
+ * @author : middlefitting
+ * @see :
+ * @since : 2023/11/02
+ */
+public class UserDao {
+
+  private ConnectionMaker connectionMaker;
+
+  public UserDao(ConnectionMaker connectionMaker) {
+    this.connectionMaker = connectionMaker;
+  }
+
+  public void addUser(User user) throws ClassNotFoundException, SQLException {
+    /**
+     * 드라이버 선택
+     */
+    Connection c = connectionMaker.getConnection();
+
+    /**
+     * SQL 생성
+     */
+    PreparedStatement ps = c.prepareStatement(
+        "insert into users(id, name, password) values(?,?,?)");
+    // SQL에 파라미터 값 넣기
+    ps.setString(1, user.getId());
+    ps.setString(2, user.getName());
+    ps.setString(3, user.getPassword());
+
+    // SQL 실행
+    ps.executeUpdate();
+    // 리소스 반환
+    ps.close();
+    // 커넥션 반환
+    c.close();
+  }
+
+  public User getUser(String id) throws ClassNotFoundException, SQLException {
+    Connection c = simpleConnectionWorker.getConnection();
+
+    PreparedStatement ps = c.prepareStatement(
+        "select * from users where id = ?");
+    ps.setString(1, id);
+
+    ResultSet rs = ps.executeQuery();
+    rs.next();
+    User user = new User();
+    user.setId(rs.getString("id"));
+    user.setName(rs.getString("name"));
+    user.setPassword(rs.getString("password"));
+
+    rs.close();
+    ps.close();
+    c.close();
+    return user;
+  }
+}
+
---
+++ spring-project/src/main/java/com/example/springproject/DAO의확장/클래스의분리/SimpleConnectionWorker.java
@@ -0,0 +1,30 @@
+package com.example.springproject.DAO의확장.클래스의분리;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import lombok.NoArgsConstructor;
+
+/**
+ * SimpleConnectionWorker.
+ *
+ * <p>
+ *  DB 연결 작업을 분리한 SimpleConnectionWorker
+ * </p>
+ *
+ * @author : middlefitting
+ * @see :
+ * @since : 2023/10/31
+ */
+@NoArgsConstructor
+public class SimpleConnectionWorker {
+  public Connection getConnection() throws ClassNotFoundException, SQLException {
+    Class.forName("com.mysql.cj.jdbc.Driver");
+
+    Connection c = DriverManager.getConnection(
+        "jdbc:mysql://localhost/tobby",
+        "middle",
+        "HelloWorld@1234");
+    return c;
+  }
+}
