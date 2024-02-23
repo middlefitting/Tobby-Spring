c8d3143bf3abea75ea171111b8d5856618cd517c
middlefitting <wjdtmdcjf2@gmail.com>
2024-02-23 22:04:45 +0900

    feat(section1_5): IOC 컨테이너

---
+++ spring-project/src/main/java/com/example/springproject/section1/section1_5/User.java
@@ -0,0 +1,22 @@
+package com.example.springproject.section1.section1_5;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * User.
+ *
+ * <p>
+ *
+ * </p>
+ * @see             : 
+ * @author          : middlefitting
+ * @since           : 2024/02/21
+ */
+@Getter
+@Setter
+public class User {
+	String id;
+	String name;
+	String password;
+}
