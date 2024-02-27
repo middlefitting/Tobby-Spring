c37b8a5c4cfb97bfe0c14b33a1d877ebb235613e
middlefitting <wjdtmdcjf2@gmail.com>
2024-02-27 22:54:59 +0900

    chore: 프로젝트 구성 변경

---
+++ src/main/java/com/example/springproject/SpringProjectApplication.java
@@ -0,0 +1,21 @@
+package com.example.springproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringProjectApplication.
+ *
+ * <p>
+ *
+ * </p>
+ * @see             : 
+ * @author          : middlefitting
+ * @since           : 2024/02/27
+ */
+@SpringBootApplication
+public class SpringProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectApplication.class, args);
+	}
+}
